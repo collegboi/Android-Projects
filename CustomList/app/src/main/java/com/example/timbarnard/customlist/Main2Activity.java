854a5b3139042829b90178af6e47f4540a084c1b
Timothy Barnard <timothy.barnard@mydit.ie>
2015-10-09 00:56:50 +0100

    CustomList
    Added Switch Screen and Send string

--- CustomList/app/src/main/java/com/example/timbarnard/customlist/Main2Activity.java
+++ CustomList/app/src/main/java/com/example/timbarnard/customlist/Main2Activity.java
@@ -1,5 +1,7 @@
 package com.example.timbarnard.customlist;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //setTitle("Timothy");
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        setTitle(message);
+
+
     }
 }
--- CustomList/app/src/main/java/com/example/timbarnard/customlist/MainActivity.java
+++ CustomList/app/src/main/java/com/example/timbarnard/customlist/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.timbarnard.customlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     int[] imageArray = {R.drawable.motorspy, R.drawable.motorspy, R.drawable.motorspy, R.drawable.motorspy,R.drawable.motorspy,R.drawable.motorspy, R.drawable.motorspy,R.drawable.motorspy};
     String[] titleArray = {"Timothy", "Stephen", "Neil","Martin","Andy","Paul","Kennan","Owen"};
@@ -25,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         listView = (ListView)findViewById(R.id.list_view);
         myadapter = new MyAdapter(getApplicationContext(), R.layout.row_layout);
         listView.setAdapter(myadapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Tities are the best", Toast.LENGTH_LONG).show();
+                Intent switchScreen = new Intent(MainActivity.this, Main2Activity.class);
+                switchScreen.putExtra(EXTRA_MESSAGE, titleArray[position]);
+                startActivity(switchScreen);
+            }
+        });
 
         for(int i = 0; i < this.imageArray.length; i++) {
             MyClass myClass = new MyClass(this.imageArray[i], this.titleArray[i], this.detailArray[i]);
